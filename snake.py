7889fb9f88ec873f78c35ea52dcb60dfd69d76fe
tagir-valitov <tagirvalitov614@gmail.com>
2023-12-31 12:09:11 +0300

    Add snake object and movement

--- snake.py
+++ snake.py
@@ -11,14 +11,22 @@ import sys
 
 app_resolution = (600, 400)
 
+
 WHITE = (255, 255, 255)
 GREY = (150, 150, 150)
 BLACK = (0, 0, 0)
 
+TIMER_EVENT = pg.USEREVENT + 1
+
 if __name__ == '__main__':
     pg.font.init()
     sc = pg.display.set_mode(app_resolution)
+    pg.time.set_timer(TIMER_EVENT, 100)  # 1 second = 1000 milliseconds
     sc.fill(GREY)
+    snake = pg.Surface((20, 20))
+    snake.fill(BLACK)
+    position = snake.get_rect()
+    sc.blit(snake, position)
     pg.display.update()
 
     while 1:
@@ -27,6 +35,10 @@ if __name__ == '__main__':
                 sys.exit()
             if i.type == pg.KEYDOWN:
                 print(i)
+            if i.type == TIMER_EVENT:
+                position = position.move(20, 0)
+                sc.fill(GREY)
+                sc.blit(snake, position)
 
         pg.display.update()
         pg.time.delay(20)
