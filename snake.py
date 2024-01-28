dab4b2fb343b5627cd12625dc916a0401fe01d37
tagir-valitov <tagirvalitov614@gmail.com>
2024-01-28 17:55:51 +0300

    Add apple

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+import random
 import pygame as pg
 import sys
 
@@ -11,24 +12,35 @@ import sys
 
 app_resolution = (600, 400)
 
-
 WHITE = (255, 255, 255)
 GREY = (150, 150, 150)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 TIMER_EVENT = pg.USEREVENT + 1
+
+
+def random_pos(rect):
+    rect.x = random.randrange(0, app_resolution[0], 20)
+    rect.y = random.randrange(0, app_resolution[1], 20)
+
 
 if __name__ == '__main__':
     pg.font.init()
     sc = pg.display.set_mode(app_resolution)
-    pg.time.set_timer(TIMER_EVENT, 100)  # 1 second = 1000 milliseconds
+    pg.time.set_timer(TIMER_EVENT, 160)  # 1 second = 1000 milliseconds
     sc.fill(GREY)
     snake = pg.Surface((20, 20))
     snake.fill(BLACK)
-    position = snake.get_rect()
-    sc.blit(snake, position)
+    snake_pos = snake.get_rect()
+    sc.blit(snake, snake_pos)
     pg.display.update()
     direction = 'r'
+    apple = pg.Surface((20, 20))
+    apple.fill(RED)
+    apple_pos = apple.get_rect()
+    random_pos(apple_pos)
+    sc.blit(apple, apple_pos)
 
     while 1:
         for i in pg.event.get():
@@ -45,26 +57,30 @@ if __name__ == '__main__':
                 if i.dict['key'] == 119:
                     direction = 'u'
             if i.type == TIMER_EVENT:
-                if position.x > app_resolution[0]:
-                    position.x = 0
-                if position.x < 0:
-                    position.x = app_resolution[0]
-                if position.y > app_resolution[1]:
-                    position.y = 0
-                if position.y < 0:
-                    position.y = app_resolution[1]
+                if snake_pos.x > app_resolution[0]:
+                    snake_pos.x = 0
+                if snake_pos.x < 0:
+                    snake_pos.x = app_resolution[0]
+                if snake_pos.y > app_resolution[1]:
+                    snake_pos.y = 0
+                if snake_pos.y < 0:
+                    snake_pos.y = app_resolution[1]
 
                 if direction == 'r':
-                    position = position.move(20, 0)
+                    snake_pos = snake_pos.move(20, 0)
                 if direction == 'l':
-                    position = position.move(-20, 0)
+                    snake_pos = snake_pos.move(-20, 0)
                 if direction == 'd':
-                    position = position.move(0, 20)
+                    snake_pos = snake_pos.move(0, 20)
                 if direction == 'u':
-                    position = position.move(0, -20)
+                    snake_pos = snake_pos.move(0, -20)
+
+                if snake_pos == apple_pos:
+                    random_pos(apple_pos)
 
                 sc.fill(GREY)
-                sc.blit(snake, position)
+                sc.blit(snake, snake_pos)
+                sc.blit(apple, apple_pos)
 
         pg.display.update()
         pg.time.delay(20)
