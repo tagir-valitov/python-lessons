a9cf363f084cfc0cc05df3d1072a62e1caf5920a
Rishat Valitov <rishat.valitov@fingerprintjs.com>
2024-03-20 16:58:32 +0300

    Adjust movement logic

--- snake.py
+++ snake.py
@@ -18,12 +18,85 @@ class Snake:
         self.surface_list = []
         surface = pg.Surface((20, 20))
         surface.fill(BLACK)
-        self.surface_list.append(surface)
-        self.pos = surface.get_rect()
+        self.surface_list.append((surface, surface.get_rect()))
+        self.direction = 'r'
 
     def blit(self):
-        for surface in self.surface_list:
-            sc.blit(surface, surface.get_rect())
+        for surface, rect in self.surface_list:
+            sc.blit(surface, rect)
+
+    def update_direction(self, keydown_key):
+        if keydown_key == 100:
+            self.direction = 'r'
+        if keydown_key == 97:
+            self.direction = 'l'
+        if keydown_key == 115:
+            self.direction = 'd'
+        if keydown_key == 119:
+            self.direction = 'u'
+
+    def get_head(self):
+        return self.surface_list[0]
+
+    def get_head_rect(self):
+        return self.surface_list[0][1]
+
+    def update_head_rect(self, rect):
+        head, head_rect = self.get_head()
+        self.surface_list[0] = (head, rect)
+
+    def update_rect(self, index, rect):
+        head, head_rect = self.get_head()
+        self.surface_list[index] = (head, rect)
+
+    def adjust_border(self):
+        _, head_rect = self.get_head()
+
+        if head_rect.x > app_resolution[0]:
+            head_rect.x = 0
+        if head_rect.x < 0:
+            head_rect.x = app_resolution[0]
+        if head_rect.y > app_resolution[1]:
+            head_rect.y = 0
+        if head_rect.y < 0:
+            head_rect.y = app_resolution[1]
+
+        self.update_head_rect(head_rect)
+
+    def move(self):
+        _, head_rect = self.get_head()
+
+        if len(self.surface_list) > 1:
+            _, tail_rect = self.surface_list[-1]
+            tail_rect = head_rect
+            self.update_rect(-1, tail_rect)
+
+        if self.direction == 'r':
+            head_rect = head_rect.move(20, 0)
+        if self.direction == 'l':
+            head_rect = head_rect.move(-20, 0)
+        if self.direction == 'd':
+            head_rect = head_rect.move(0, 20)
+        if self.direction == 'u':
+            head_rect = head_rect.move(0, -20)
+
+        self.update_head_rect(head_rect)
+
+    def add_surface(self):
+        surface = pg.Surface((20, 20))
+        surface.fill(BLACK)
+        rect = self.surface_list[-1][1]
+
+        if self.direction == 'r':
+            rect = rect.move(-20, 0)
+        if self.direction == 'l':
+            rect = rect.move(20, 0)
+        if self.direction == 'd':
+            rect = rect.move(0, -20)
+        if self.direction == 'u':
+            rect = rect.move(0, 20)
+
+        self.surface_list.append((surface, rect))
 
 
 class Apple:
@@ -54,64 +127,40 @@ RED = (255, 0, 0)
 TIMER_EVENT = pg.USEREVENT + 1
 
 
-def random_pos(rect):
-    rect.x = random.randrange(0, app_resolution[0], 20)
-    rect.y = random.randrange(0, app_resolution[1], 20)
-
-
 if __name__ == '__main__':
     pg.font.init()
     sc = pg.display.set_mode(app_resolution)
     pg.time.set_timer(TIMER_EVENT, 160)
     sc.fill(GREY)
+
     snake = Snake()
-    snake.blit()
-    pg.display.update()
-    direction = 'r'
     apple = Apple()
+
+    snake.blit()
     apple.blit()
+
+    pg.display.update()
 
     while 1:
         for i in pg.event.get():
             if i.type == pg.QUIT:
                 sys.exit()
             if i.type == pg.KEYDOWN:
-                print(i)
-                if i.dict['key'] == 100:
-                    direction = 'r'
-                if i.dict['key'] == 97:
-                    direction = 'l'
-                if i.dict['key'] == 115:
-                    direction = 'd'
-                if i.dict['key'] == 119:
-                    direction = 'u'
+                key = i.dict['key']
+                snake.update_direction(key)
             if i.type == TIMER_EVENT:
-                if snake.pos.x > app_resolution[0]:
-                    snake.pos.x = 0
-                if snake.pos.x < 0:
-                    snake.pos.x = app_resolution[0]
-                if snake.pos.y > app_resolution[1]:
-                    snake.pos.y = 0
-                if snake.pos.y < 0:
-                    snake.pos.y = app_resolution[1]
-
-                if direction == 'r':
-                    snake.pos.move(20, 0)
-                if direction == 'l':
-                    snake.pos = snake.pos.move(-20, 0)
-                if direction == 'd':
-                    snake.pos = snake.pos.move(0, 20)
-                if direction == 'u':
-                    snake.pos = snake.pos.move(0, -20)
-
                 sc.fill(GREY)
 
-                if snake.pos == apple.get_rect():
+                snake.adjust_border()
+                snake.move()
+
+                if snake.get_head_rect() == apple.get_rect():
                     apple.random_pos()
+                    snake.add_surface()
 
                 apple.blit()
                 snake.blit()
 
-        pg.display.update()
+        pg.display.flip()
         pg.time.delay(20)
         
